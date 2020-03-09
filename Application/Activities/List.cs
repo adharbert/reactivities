3faa62eec3705d6f7ffaa746bbdd121ab333176c
adharbert <adharbert@gmail.com>
2020-03-09 18:00:51 -0400

    adding cqrs code and activity service

---
+++ Application/Activities/List.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> {}
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            // NOTE: more info on cancellation token, view sec 4#43
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await _context.Activities.ToListAsync();
+                return activities;
+            }
+        }
+    }
+}
